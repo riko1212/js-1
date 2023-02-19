865fd1f301f3f48957e6affd76dc1ed0095bf7c6
Andrii Danylko <46224717+riko1212@users.noreply.github.com>
2023-02-19 19:37:21 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,31 @@
+const boxes = document.querySelector('#boxes');
+const boxesCountInput = document.querySelector('#controls > input');
+const createBoxBtn = document.querySelector('[data-create]')
+const destroyBoxBtn = document.querySelector('[data-destroy]')
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const createBoxes = (amount) => {
+  destroyBoxes()
+  let baseSize = 20;
+  for(let i = 1; i<=amount; i+=1) {
+    const el = document.createElement('div');
+    baseSize+=10
+    el.style.width = baseSize+'px';
+    el.style.height = el.style.width;
+    el.style.backgroundColor = getRandomHexColor();
+    boxes.appendChild(el);
+    
+  }
+}
+
+const destroyBoxes = () => {
+  boxes.innerHTML = ''
+}
+
+createBoxBtn.addEventListener('click', () => {
+  createBoxes(Number(boxesCountInput.value))
+})
+destroyBoxBtn.addEventListener('click', destroyBoxes)
